793a3e13687cb8808ef17cdd96cbf18826352b98
huangfuyao <huangfuyao@meituan.com>
2018-04-10 15:04:06 +0800

    111

---
+++ src/main/java/cn/benhuangfuyao/reflection/RetrievingClassObjects.java
@@ -0,0 +1,24 @@
+package cn.benhuangfuyao.reflection;
+
+public class RetrievingClassObjects {
+
+    public enum Light {
+        RED , GREEN , YELLOW ;
+    }
+    public static void main(String[] args) {
+
+
+//        Class c = Light.RED.getClass();
+
+        byte[] bytes = new byte[1024];
+        Class c = bytes.getClass();
+
+        System.out.println(c.getName());
+
+
+
+    }
+}
+
+
+
